0478bea62dce64b1bb3140cea937c810f70ed77c
chandlerkeyes <chandlerkeyes@yahoo.com>
2023-09-21 17:55:04 -0500

    connected to mongodb

--- index.ts
+++ index.ts
@@ -1,10 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import * as mongoose from "mongoose";
 
 dotenv.config();
 
+async function connect() {
+    try {
+        await mongoose.connect(String(process.env.DB_URI))
+        console.log("Connected to MongoDB");
+    } catch (e) {
+        console.error(e);
+    }
+}
+
 const app: Express = express();
 const port = process.env.PORT;
+
+connect();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
